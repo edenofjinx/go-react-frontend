5428b1dcc02ec4c1738fd09027f58a141cf5a7c6
Julius Liaudanskis <julius.l@emotion.lt>
2021-11-27 20:39:28 +0200

    go-movies: 31

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,31 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 
 export default class Movies extends Component {
 
+    state = {movies: []};
+
+    componentDidMount() {
+        this.setState({
+            movies: [
+                {id: 1, title: "Titanic", runtime: 142},
+                {id: 2, title: "The fight club", runtime: 125},
+                {id: 3, title: "The matrix", runtime: 118}
+            ]
+        })
+    }
+
     render() {
         return (
-            <h2>Choose a movie</h2>
+            <Fragment>
+                <h2>Movie homepage</h2>
+                <ul>
+                    {this.state.movies.map( (m) => (
+                        <li key={m.id}>
+                            {m.title}
+                        </li>
+                    ))}
+                </ul>
+            </Fragment>
         );
     }
 }
