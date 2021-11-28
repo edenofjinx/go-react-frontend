db1a93ad291404aee326e8fe48356259ba808d95
Julius Liaudanskis <julius.l@emotion.lt>
2021-11-28 16:16:58 +0200

    go-movies: 56

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Admin from "./components/Admin";
 import Home from "./components/Home";
 import Genres from "./components/Genres";
 import OneMovie from "./components/OneMovie";
+import OneGenre from "./components/OneGenre";
 
 function App() {
   return (
@@ -41,6 +42,7 @@ function App() {
                 <Route path="/movies">
                   <Movies />
                 </Route>
+                <Route path="/genre/:id" component={OneGenre} />
                 <Route exact path="/genres">
                   <Genres />
                 </Route>
