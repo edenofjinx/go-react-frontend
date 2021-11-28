0c2e2ac113ca6be452e29fe7d9d7a9a6e74b775d
Julius Liaudanskis <julius.l@emotion.lt>
2021-11-28 14:40:02 +0200

    go-movies: 50

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,30 +5,49 @@ export default class Movies extends Component {
 
     state = {
         movies: [],
-        isLoaded: false
+        isLoaded: false,
+        error: null
     };
 
     componentDidMount() {
         fetch("http://localhost:4000/v1/movies")
-            .then((response) => response.json())
+            // .then((response) => response.json())
+            .then((response) => {
+                console.log("status code is", response.status)
+                if (response.status !== "200") {
+                    let err = Error;
+                    err.message = "Invalid response code: " + response.status
+                    this.setState({error: err})
+                }
+                return response.json();
+            })
             .then((json) => {
                 this.setState({
-                    movies: json.movies,
-                    isLoaded: true
-                })
-            })
+                        movies: json.movies,
+                        isLoaded: true
+                    },
+                    (error) => {
+                        this.setState({
+                            isLoaded: true,
+                            error
+                        });
+                    }
+                );
+            });
     }
 
     render() {
-        const {movies, isLoaded} = this.state;
-        if (!isLoaded) {
+        const {movies, isLoaded, error} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
             return <p>Loading...</p>
         } else {
             return (
                 <Fragment>
                     <h2>Movie homepage</h2>
                     <ul>
-                        {movies.map( (m) => (
+                        {movies.map((m) => (
                             <li key={m.id}>
                                 <Link to={`/movies/${m.id}`}>
                                     {m.title}
