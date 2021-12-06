25fc7e0f362b53898010843f9b1bd4d2383eca2a
Julius Liaudanskis <julius.l@emotion.lt>
2021-12-06 15:32:48 +0200

    go-movies: 87

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React, {Component} from "react";
+import Ticket from "./../images/movie_tickets.jpg"
+import "./Home.css";
 
 export default class Home extends Component {
 
     render() {
         return (
-            <h2>Movie homepage</h2>
+            <div className="text-center">
+                <h2>Movie homepage</h2>
+                <hr/>
+                <img src={Ticket} alt="Movie ticket"/>
+                <hr/>
+                <div className="tickets"></div>
+            </div>
         );
     }
 }
